ac12a2facb15a6f641b91439086f2855d5747d37
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-09-09 16:58:51 +0530

    Dynamic Routing concept

---
+++ dynamicroute_profiles/src/app/users/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserProfile = ({params} : {params: {id:string}}) => {
+  const {id} =params;
+  return (
+    <div>
+      <h1>UserProfile : {id}</h1>
+    </div>
+  )
+}
+
+export default UserProfile
